09172949c7ca6a85f407af7a64fb264ab3f339b6
LuckyEvGen <luckybird.work@gmail.com>
2025-01-06 14:42:52 +0300

    2

--- module_8_1.py
+++ module_8_1.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+def add_everything_up(a, b):
+    try:
+        return round(a + b, 3)
+    except TypeError as exc:
+        return str(a) + str(b)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
